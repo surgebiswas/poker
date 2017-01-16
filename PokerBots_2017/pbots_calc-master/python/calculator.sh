6d55f16d88798df98d1a53d17d9449c9825afcae
amarotaylor <amaro.taylor@gmail.com>
2017-01-16 14:35:01 -0500

    built hand evaluator
    pbots-calc_master

---
+++ PokerBots_2017/pbots_calc-master/python/calculator.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export DYLD_LIBRARY_PATH=/Users/amaro/GitHub/poker/PokerBots_2017/pbots_calc-master/export/darwin/lib:$LD_LIBRARY_PATH
+python /Users/amaro/GitHub/poker/PokerBots_2017/pbots_calc-master/python/calculator.py $@
---
+++ PokerBots_2017/sample_bot/Player.py
@@ -0,0 +1,61 @@
+import argparse
+import socket
+import sys
+
+"""
+Simple example pokerbot, written in python.
+
+This is an example of a bare bones pokerbot. It only sets up the socket
+necessary to connect with the engine and then always returns the same action.
+It is meant as an example of how a pokerbot should communicate with the engine.
+"""
+class Player:
+    def run(self, input_socket):
+        # Get a file-object for reading packets from the socket.
+        # Using this ensures that you get exactly one packet per read.
+        f_in = input_socket.makefile()
+        while True:
+            # Block until the engine sends us a packet.
+            data = f_in.readline().strip()
+            # If data is None, connection has closed.
+            if not data:
+                print "Gameover, engine disconnected."
+                break
+
+            # Here is where you should implement code to parse the packets from
+            # the engine and act on it. We are just printing it instead.
+            print data
+
+            # When appropriate, reply to the engine with a legal action.
+            # The engine will ignore all spurious responses.
+            # The engine will also check/fold for you if you return an
+            # illegal action.
+            # When sending responses, terminate each response with a newline
+            # character (\n) or your bot will hang!
+            word = data.split()[0]
+            if word == "GETACTION":
+                # Currently CHECK on every move. You'll want to change this.
+                s.send("CHECK\n")
+            elif word == "REQUESTKEYVALUES":
+                # At the end, the engine will allow your bot save key/value pairs.
+                # Send FINISH to indicate you're done.
+                s.send("FINISH\n")
+        # Clean up the socket.
+        s.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='A Pokerbot.', add_help=False, prog='pokerbot')
+    parser.add_argument('-h', dest='host', type=str, default='localhost', help='Host to connect to, defaults to localhost')
+    parser.add_argument('port', metavar='PORT', type=int, help='Port on host to connect to')
+    args = parser.parse_args()
+
+    # Create a socket connection to the engine.
+    print 'Connecting to %s:%d' % (args.host, args.port)
+    try:
+        s = socket.create_connection((args.host, args.port))
+    except socket.error as e:
+        print 'Error connecting! Aborting'
+        exit()
+
+    bot = Player()
+    bot.run(s)
