1bcf4887c313fd710e8bab76c7fe74d4951efe00
amarotaylor <amaro.taylor@gmail.com>
2017-01-24 22:00:55 -0500

    update removing extra folders and adding discard / initial hand EV data

--- PokerBots_2017/pbots_calc-master/python/calculator.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export DYLD_LIBRARY_PATH=/Users/amaro/GitHub/poker/PokerBots_2017/pbots_calc-master/export/darwin/lib:$LD_LIBRARY_PATH
-python /Users/amaro/GitHub/poker/PokerBots_2017/pbots_calc-master/python/calculator.py $@
